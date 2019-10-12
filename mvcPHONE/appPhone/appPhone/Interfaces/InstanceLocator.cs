fff543db0c1c219b15ca98ea53650a441e58291d
visual_community@outlook.com <visual_community@outlook.com>
2019-10-12 13:16:33 -0400

    MVVM completo

---
+++ mvcPHONE/appPhone/appPhone/Interfaces/InstanceLocator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appPhone.Interfaces
+{
+    using ViewModels;//sale rojo porque no esta creado todavia el ViewModels
+    public class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion  
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
