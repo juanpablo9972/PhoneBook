e1b333877793a25fabf4ef45acb002939270f1a4
visual_community@outlook.com <visual_community@outlook.com>
2019-11-23 21:55:14 -0300

    Test

---
+++ mvcPHONE/Test_MVVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test_MVVC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test_MVVC")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9406eba6-2954-4d54-8325-543d5ccd285d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ mvcPHONE/Test_MVVC/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_MVC
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestControllerExistePhoneExists()
+        {
+            var controller = new apiPhone.Controllers.PhonesController();
+
+            bool response = controller.PhoneExists(-1);
+
+            Assert.AreEqual(false, response);
+        }
+    }
+}
